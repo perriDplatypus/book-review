6856157a5a581383eabb82507454cc2b9919da35
abhay <abhaytyagi2513@gmail.com>
2025-02-09 21:17:43 +1100

    Added controller and made views for list books

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $title = $request->input(key: 'title');
+        $books = Book::when(
+            value: $title,
+            callback: fn(Builder $query, $title): mixed => $query->title($title)
+        )->get();
+
+        return view(view: 'books.index', data: ['books' => $books]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): void
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): void
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): void
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id): void
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): void
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): void
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Route;
 
 Route::get(uri: '/', action: function (): View {
     return view(view: 'welcome');
 });
+
+Route::resource(name: 'books', controller: BookController::class);
