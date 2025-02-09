1a7e869d96337183c2032e58ae04e330e00e618a
abhay <abhaytyagi2513@gmail.com>
2025-02-09 21:26:50 +1100

    Added search form

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,12 @@
 @section('content')
     <h1 class="mb-10 text-2xl">Books</h1>
 
-    <form></form>
+    <form method="GET" action="{{ route(name: 'books.index') }}" class="mb-4 flex items-center space-x-2">
+        <input type="text" name="title" class="input h-10" placeholder="Search by title.."
+            value="{{ request(key: 'title') }}" />
+        <button type="submit" class="btn h-10">Search</button>
+        <a href="{{ route(name: 'books.index') }}" class="btn h-10">Clear</a>
+    </form>
 
     <ul>
         @forelse ($books as $book)
