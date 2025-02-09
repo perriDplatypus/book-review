1ca9cdcb902dac402b1ba0cec96b9060540cafe4
abhay <abhaytyagi2513@gmail.com>
2025-02-09 17:32:07 +1100

    Fixed seeding issue where updated_at was before created_at

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -16,11 +16,13 @@ class BookFactory extends Factory
      */
     public function definition(): array
     {
+        $created_at = fake()->dateTimeBetween('-2 years');
+        $updated_at = fake()->dateTimeBetween($created_at, 'now');
         return [
             'title' => fake()->sentence(3),
             'author' => fake()->name,
-            'created_at' => fake()->dateTimeBetween('-2 years'),
-            'updated_at' => fake()->dateTimeBetween('created_at', 'now')
+            'created_at' => $created_at,
+            'updated_at' => $updated_at
         ];
     }
 }
