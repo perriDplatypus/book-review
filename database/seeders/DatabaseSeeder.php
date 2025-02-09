ceba54d38945862b74d4f2013eaa84f7e19566af
abhay <abhaytyagi2513@gmail.com>
2025-02-09 16:59:14 +1100

    Created factories and seeded data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Book;
+use App\Models\Review;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // generate books with good reviews
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)->good()->for($book)->create();
+        });
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // generate books with average reviews
+        Book::factory(33)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)->average()->for($book)->create();
+        });
+
+        // generate books with bad reviews
+        Book::factory(34)->create()->each(function ($book) {
+            $numReviews = random_int(5, 30);
+            Review::factory()->count($numReviews)->bad()->for($book)->create();
+        });
     }
 }
