93c9dba443f1b7867e81cd00fdda0999ebbbd96b
abhay <abhaytyagi2513@gmail.com>
2025-02-09 17:30:07 +1100

    Fixed seeding issue where updated_at was before created_at

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -16,12 +16,14 @@ class ReviewFactory extends Factory
      */
     public function definition(): array
     {
+        $created_at = fake()->dateTimeBetween('-2 years');
+        $updated_at = fake()->dateTimeBetween($created_at, 'now');
         return [
             'book_id' => null,
             'review' => fake()->paragraph(),
             'rating' => fake()->numberBetween(1, 5),
-            'created_at' => fake()->dateTimeBetween('-2 years'),
-            'updated_at' => fake()->dateTimeBetween('created_at', 'now')
+            'created_at' => $created_at,
+            'updated_at' => $updated_at
         ];
     }
 
